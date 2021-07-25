88219cc8603a8a069c58c7d362900d37c696cb2e
Azeempasha471 <abdulazeem1023@gmail.com>
2021-07-26 00:41:45 +0530

    Added:Validating firstname

--- com/userregistration/UserRegistration.java
+++ com/userregistration/UserRegistration.java
@@ -1,9 +1,32 @@
 package com.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+	// Scanner class for user input
+	Scanner sc = new Scanner(System.in);
 
-		public static void main(String[] args) {
-			System.out.println("welcome to user registration");
+	// method to check firstname Valid or Invalid
+	public void validateUsername() {
+		System.out.println("Enter UserName");
+		String userName = sc.next();
+		// regex pattern for firstname
+		String regex = "^[A-Z]{1}[a-zA-z0-9]{2,}$";
+		Pattern p = Pattern.compile(regex);
+		Matcher matcher = p.matcher(userName);
+		boolean result = matcher.matches();
+
+		if (result) {
+			System.out.println("Valid username");
+		} else {
+			System.out.println("Invalid username");
 		}
+	}
 
+	public static void main(String[] args) {
+		UserRegistration registration = new UserRegistration();
+		registration.validateUsername();
+	}
 }
